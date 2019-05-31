9fc021b7f350f53eb460a3899c6dc32b73098cde
Jerry L Hoover <jerry.hoover@ymail.com>
2019-05-31 18:17:19 -0500

    May 31 commit

--- js/app.js
+++ js/app.js
@@ -86,9 +86,9 @@ function createLineChart() {
       }
    });
 }
-// =-=-=-=-=-=-=-=-=-=-=-= END LINE CHART =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// =-=-=--=-=-=-=-=-= END LINE CHART =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-// =-=-=-=-=-=-=-=-=-=-=-=-= BAR CHART =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// =-=-=-=-=-=-=-=-=-=-= BAR CHART =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 function createBarChart() {
    let chartLabel = ["S", "M", "T", "W", "T", "F", "S"];
    let chartData = [75, 100, 175, 125, 225, 200, 100];
@@ -136,9 +136,9 @@ function createBarChart() {
       }
    });
 }
-// =-=-=-=-=-=-=-=-=-=-= END BAR CHART =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// =-=-=-=-=-=-=-=-=-=-= END BAR CHART =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-// =-=-=-=-=-=-=-=-=-=-= MOBILE USER CHART =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// =-=-=-=-=-=-=-= MOBILE USER CHART =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 function createDonutChart() {
    //
    let chartLabel = ["Phones", "Tablets", "Desktop"];
@@ -176,17 +176,100 @@ function createDonutChart() {
       }
    });
 }
-// =-=-=-=-=-=-=-=-=-=-=-=-= END OF MOBILE USER CHART =-=-=-=-=-=-=-=-=-=-=-=-=
+// =-=-=-=-=-=-=-=-=--= END OF MOBILE USER CHART =-=-=-=-=-=-=-=-=-=-=-=-=
+
+function new_members() {
+   const newMemberData = [
+      {
+         name: "Victoria Chambers",
+         image: "../images/member-1.jpg",
+         email: "victoria.chambers80@example.com",
+         date: "10/15/15",
+         message: "commented on YourApp's SEO Tips",
+         time: "4 hours ago"
+      },
+      {
+         name: "Dale Byrd",
+         image: "images/member-2.jpg",
+         email: "dale.byrd52@example.com",
+         date: "10/15/15",
+         message: "liked the post Facebook's Changes for 2016",
+         time: "5 hours ago"
+      },
+      {
+         name: "Dawn Wood",
+         image: "images/member-3.jpg",
+         email: "dawn.wood16@example.com",
+         date: "10/15/15",
+         message: "commented on Facebook's Changes for 2016",
+         time: "5 hours ago"
+      },
+      {
+         name: "Dan Oliver",
+         image: "images/member-4.jpg",
+         email: "dan.oliver82@example.com",
+         date: "10/14/15",
+         message: "posted YourApp's SEO Tips",
+         time: "1 day ago"
+      }
+   ];
+   // to modularize this
+   // create func with three lines
+   // 0. pass ??? where to insert
+   // 1. createElement (pass div or p)
+   // 2. assign class name (pass class)
+   // 3. textContent (pass newMemberData[i] and name or email or date etc)
+   // 4. appendChild
+   // example function stickInDOM ("div", "member", newMemberData[i], "email");
+
+   // for loop goes here
+   // get target div conatiner
+   let new_member_wrapper = document.getElementById("new-member-wrapper");
+
+   // create new div to hold members
+   let member_div = document.createElement("div");
+   // member_div.setAttribute("class", "member");
+   new_member_wrapper.appendChild(member_div);
+
+   // add member image
+   let member_image = document.createElement("img");
+   member_image.setAttribute("src", newMemberData[0].image); // loop fix
+   // assign class
+   member_div.appendChild(member_image);
+
+   // add name div
+   let name_div = document.createElement("div");
+   // assign class
+   member_div.appendChild(name_div);
+
+   // insert name in name div
+   let member_name = document.createElement("p");
+   // assign class
+   member_name.textContent = newMemberData[0].name; // loop fix
+   name_div.appendChild(member_name);
+
+   //insert email in name div
+   let member_email = document.createElement("p");
+   // assign class
+   member_email.textContent = newMemberData[0].email; //loop fix
+   name_div.appendChild(member_email);
+
+   // insert date
+   let member_date = document.createElement("p");
+   // assign class
+   member_date.textContent = newMemberData[0].date; // loop fix
+   name_div.appendChild(member_date);
+}
 
 document.addEventListener("click", event => {
    if (event.target.className == "nav-button") {
-      // =-=-=-=-=-= TURN OFF ALL INDICATORS =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+      // =-=-=-=-=-= TURN OFF ALL INDICATORS =-=-=-=-=-=-=-=-=-=-=-=
       let nav_button = document.getElementsByClassName("nav-button");
       for (let i = 0; i < 4; i++) {
          nav_button[i].style.borderLeftStyle = "none";
          nav_button[i].style.opacity = "0.4";
       }
-      // =-=-=-=-=-=-= TURN ON THE DESIRED INDICATOR =-=-=-=-=-=-=-=-=-=-=-=-=
+      // =-=-=-=-=-=-= TURN ON THE DESIRED INDICATOR =-=-=-=-=-=-=-=
       event.target.style.borderLeft = "3px solid #82d67d";
       event.target.style.borderRadius = "3px";
       event.target.style.opacity = "1";
@@ -195,14 +278,14 @@ document.addEventListener("click", event => {
 
 document.addEventListener("mouseover", event => {
    if (event.target.className == "nav-button") {
-      //  =-=-=-=-=-=-=-=-=-=-=-= TURN OFF ALL INDICATORS =-=-=-=-=-=-=-=-=-=-=
+      //  =-=-=-=-=-=-=-=-=-= TURN OFF ALL INDICATORS =-=-=-=-=-=-=
       let nav_button = document.getElementsByClassName("nav-button");
       for (let i = 0; i < 4; i++) {
          if (nav_button[i].style.borderLeftStyle !== "solid") {
             nav_button[i].style.opacity = "0.4";
          }
       }
-      // =-=-=-=-=-=-=-=-=-= TURN ON TARGETED INDICATOR =-=-=-=-=-=-=-=-=-=-=-=
+      // =-=-=-=-=-=-=-= TURN ON TARGETED INDICATOR =-=-=-=-=-=-=
       event.target.style.opacity = "1";
    }
 });
@@ -217,10 +300,11 @@ document.addEventListener("mouseout", event => {
    }
 });
 
-// =-=-=-=-=-=-=-=-=-=-= MAIN PROCEDURE =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// =-=-=-=-=-=-=-= MAIN PROCEDURE =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 createLineChart();
 createBarChart();
 createDonutChart();
+new_members();
 
 window.onload = function() {
    if (first_run_flag) {
